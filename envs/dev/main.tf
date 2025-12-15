decc7257d62631c2640cf77528a2c3ec0753f2b3
k-kbk <kkbk0077@gmail.com>
2025-12-15 15:31:41 +0900

    test: ci test

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,11 +1,5 @@
 provider "aws" {
-  region     = "ap-northeast-2"
-  access_key = "mock_access_key"
-  secret_key = "mock_secret_key"
-
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-  skip_metadata_api_check     = true
+  region = "ap-northeast-2"
 }
 
 module "s3_bucket" {
