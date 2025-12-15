2aac286d5704316f8f87fb3272d97bfc5948d126
k-kbk <kkbk0077@gmail.com>
2025-12-15 15:14:10 +0900

    test: ci test

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "ap-northeast-2"
+
+
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+}
+
+module "s3_bucket" {
+  source = "../../modules/simple_bucket"
+
+  bucket_name = "test-ci-bucket-2025-12-15"
 }
