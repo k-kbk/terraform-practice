e04ace25dc49efd1668e26994adaa25ce3abeb3f
k-kbk <kkbk0077@gmail.com>
2025-12-16 13:12:15 +0900

    fix: fix issue

--- envs/dev/network.tf
+++ envs/dev/network.tf
@@ -29,7 +29,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = cidrsubnet(var.vpc_cidr, var.public_subnet_newbits, count.index)
   availability_zone       = element(data.aws_availability_zones.available.names, count.index)
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = merge(local.common_tags, {
     Name = "${local.name_prefix}-public-${count.index + 1}"
